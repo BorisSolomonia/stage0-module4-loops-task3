2c8bd127a6f958e50c883a3d2952373fd5cf5404
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-20 19:56:36 +0400

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int f1 = 0, f2 = 1, i;
+
+        if (lastFibonacci < 1)
+            return;
+        System.out.print(f1 + " ");
+        for (i = 1; i < lastFibonacci; i++)
+        {
+            System.out.print(f2 + " ");
+            int next = f1 + f2;
+            f1 = f2;
+            f2 = next;
+        }
     }
 }
