c454c117b4906ef5bad3a36cbd918c90f37191c2
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-20 19:53:21 +0400

    Update NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int n = 0;
+        String nines = "";
+        for (int i = 0; i < lengthOfLastNumber; i++) {
+            nines +="9";
+            n += Integer.parseInt(nines);
+        }
+        System.out.println(n);
     }
 }
