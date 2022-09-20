d07d17102b1cafa15de2ac8739c23360168f294e
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-20 16:18:26 +0400

    Update RangeSum.java

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        int n = 0;
+        for (int i = firstBoarder+1; i < secondBoarder; i++) {
+            n += i;
+        }
+        System.out.println(n);
     }
 }
