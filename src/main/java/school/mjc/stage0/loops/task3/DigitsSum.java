ded5ef43d7ec9994b6e1cfb7f82667c42392214f
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-20 19:37:34 +0400

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        String len = String.valueOf(t);
+        int n = 0;
+        int k = 0;
+        while (n < len.length()){
+            k += Integer.parseInt(String.valueOf(len.charAt(n)));
+            n++;
+        }
+        System.out.println(k);
     }
 }
