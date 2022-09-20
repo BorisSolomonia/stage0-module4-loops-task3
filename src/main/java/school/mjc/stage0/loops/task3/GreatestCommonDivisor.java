64187feda40ecce40921b9ac0b6caadf0ba33c8a
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-20 19:21:52 +0400

    Update GreatestCommonDivisor.java

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int n = Math.min(first, second);
+        int f = Math.max(first, second);
+        int r = n;
+        int k = 1;
+        while (k != 0){
+            n--;
+            k = f%n + r%n;
+            System.out.println(n);
+        }
+        System.out.println("the last n "+n);
     }
 }
